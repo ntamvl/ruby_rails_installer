dbf09d12b599b8f2f2975785e8f210a96c5c11f1
Tam Nguyen <ntamvl@gmail.com>
2018-08-20 11:54:17 +0700

    add install redis

--- install-macos.sh
+++ install-macos.sh
@@ -51,4 +51,9 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo -e "Install vimrc completed. \n\n"
 
+echo -e "Install Redis"
+brew install redis
+brew services start redis
+echo -e "Install Redis completed."
+
 echo -e "\n\nDone. \n\n"
