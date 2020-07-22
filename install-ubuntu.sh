e8a6a3a4341849a486b394515f88fd0ba7bccdc1
Tam Nguyen <ntamvl@gmail.com>
2020-07-22 18:25:15 +0700

    update script ubuntu

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -6,11 +6,15 @@ RAILS_VERSION=6.0.3.2
 echo "Install recommend packages"
 sudo apt update
 sudo apt -y install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt -y install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 echo "Install recommend packages completed. \n\n"
 
 echo "Installing rbenv..."
