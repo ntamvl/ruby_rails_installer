6cd06af71d3529cb71a64ef94f4c59f639d60cc7
Tam Nguyen <ntamvl@gmail.com>
2020-07-22 18:30:17 +0700

    update command for yarn

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -7,8 +7,8 @@ echo "Install recommend packages"
 sudo apt update
 sudo apt -y install curl
 
-# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 # curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
