589ef6b16a0764531379e20ac916c331a648fa4f
Tam Nguyen <ntamvl@gmail.com>
2021-02-03 21:50:44 +0700

    update ruby 3.0.0 and rails 6.1.1, add ohmyzsh

--- install-macos.sh
+++ install-macos.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e 
 
-RUBY_VERSION=2.7.1
-RAILS_VERSION=6.0.3.2
+set -e
+
+RUBY_VERSION=3.0.0
+RAILS_VERSION=6.1.1
 
 echo "Install homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -9,7 +11,7 @@ echo "Install homebrew completed. \n\n"
 
 echo "Installing rbenv..."
 brew install rbenv ruby-build
-echo -e "Install rbenv done. \n\n"
+echo "Install rbenv done. \n\n"
 
 # Add rbenv to bash so that it loads every time you open a terminal
 echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
@@ -19,7 +21,7 @@ echo "Installing Ruby ${RUBY_VERSION}..."
 rbenv install $RUBY_VERSION
 rbenv global $RUBY_VERSION
 ruby -v
-echo -e "Installed Ruby ${RUBY_VERSION} done. \n\n"
+echo "Installed Ruby ${RUBY_VERSION} done. \n\n"
 
 git config --global color.ui true
 
@@ -27,11 +29,11 @@ echo "Installing Rails ${RAILS_VERSION}..."
 gem install rails -v $RAILS_VERSION
 rbenv rehash
 rails -v
-echo -e "Install Rails ${RAILS_VERSION} done. \n\n"
+echo "Install Rails ${RAILS_VERSION} done. \n\n"
 
 echo "Installing PostgreSQL..."
 brew install postgresql
-echo -e "Install PostgreSQL done. \n\n"
+echo "Install PostgreSQL done. \n\n"
 
 echo "Starting PostgreSQL..."
 brew services start postgresql
@@ -41,9 +43,9 @@ brew services start postgresql
 # To access from terminal
 # psql -d postgres 
 # or psql -d template1
-echo -e "Start PostgreSQL completed. \n\n"
+echo "Start PostgreSQL completed. \n\n"
 
-echo -e "Install git bash-completion"
+echo "Install git bash-completion"
 brew install git bash-completion
 # Add bash-completion to your ~/.bash_profile:
 # [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
@@ -52,27 +54,35 @@ echo -e "Install git bash-completion completed. \n\n"
 echo -e "Install vimrc"
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-echo -e "Install vimrc completed. \n\n"
+echo "Install vimrc completed. \n\n"
 
-echo -e "Install Redis"
+echo "Install Redis"
 brew install redis
 brew services start redis
-echo -e "Install Redis completed."
+echo "Install Redis completed."
 
-echo -e "Install tmux"
+echo "Install tmux"
 brew install tmux
-echo -e "Install tmux completed."
+echo "Install tmux completed."
 
-echo -e "Install htop"
+echo "Install htop"
 brew install htop
-echo -e "Install htop completed."
+echo "Install htop completed."
 
-echo -e "Install ffmpeg"
+echo "Install ffmpeg"
 brew install ffmpeg
-echo -e "Install ffmpeg completed."
+echo "Install ffmpeg completed."
 
-echo -e "Install youtube-dl"
+echo "Install youtube-dl"
 brew install youtube-dl
-echo -e "Install youtube-dl completed."
+echo "Install youtube-dl completed."
 
-echo -e "\n\nDone. \n\n"
+echo "Install ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Install ohmyzsh completed."
+
+echo "Install powerlevel10k for ohmyzsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Install powerlevel10k for ohmyzsh completed."
+
+echo "\n\nDone. \n\n"
