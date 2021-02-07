495759c32fa2d6efa17e33ba93b96443e4d7fbbb
Tam Nguyen <ntamvl@gmail.com>
2021-02-07 10:15:58 +0700

    update command

--- install-macos.sh
+++ install-macos.sh
@@ -84,16 +84,16 @@ echo "Install midnight commander completed."
 echo "Install ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Install powerlevel10k for ohmyzsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo "Install powerlevel10k for ohmyzsh completed."
+
 echo "Download zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 echo "Download zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 echo "Install ohmyzsh completed."
-
-echo "Install powerlevel10k for ohmyzsh"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-echo "Install powerlevel10k for ohmyzsh completed."
 
 echo -e "\n\nDone. \n\n"
