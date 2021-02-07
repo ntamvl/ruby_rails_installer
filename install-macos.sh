efcdd7d77525a983d66cbc9c58abedbb401e366e
Tam Nguyen <ntamvl@gmail.com>
2021-02-07 12:06:13 +0700

    update command

--- install-macos.sh
+++ install-macos.sh
@@ -87,13 +87,22 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "Install powerlevel10k for ohmyzsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+source ~/.zshrc
 echo "Install powerlevel10k for ohmyzsh completed."
 
-echo "Download zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# echo "Download zsh-autosuggestions"
+# git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-echo "Download zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-echo "Install ohmyzsh completed."
+# echo "Download zsh-syntax-highlighting"
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# echo "Install ohmyzsh completed."
+
+echo "Install node"
+brew install node
+echo "Install node completed."
+
+echo "Install yarn"
+brew install yarn
+echo "Install yarn completed."
 
 echo -e "\n\nDone. \n\n"
